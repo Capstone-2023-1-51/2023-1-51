cd522c2b2b14d4e391136c8b1951a8fc6d6ecdcb
ziwone <jion3422@pusan.ac.kr>
2023-07-30 15:42:08 +0900

    바이트 코드 이미지 변환 작업 업로드

---
+++ main.py
@@ -0,0 +1,7 @@
+from compileSol import compile_solidity
+#from createImage import bytecode_to_image
+
+folder_path = 'E:/sol'
+compile_solidity(folder_path)
+#folder_path = 'E:/sol/bin'
+#bytecode_to_image(folder_path)
